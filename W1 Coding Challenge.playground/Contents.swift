bf749ab25689c63e1a867131d30644da10b546e0
Sam Wilskey <swilskey41@gmail.com>
2015-08-04 10:38:02 -0700

    Finished Week Challenges

--- W1 Coding Challenge.playground/Contents.swift
+++ W1 Coding Challenge.playground/Contents.swift
@@ -55,8 +55,75 @@ fizzbuzz()
 *
 */
 
-func hiCount(string: String) {
-    
+func hiCount(string: String){
+  let hi = "hi"
+  var words = string.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+  var wordDictionary = [String: Int]()
+  
+  for word in words {
+    if let count = wordDictionary[word] {
+      wordDictionary[word] = count + 1
+    } else {
+      wordDictionary[word] = 1
+    }
+  }
+  println("There are \(wordDictionary[hi]!) hi's in the string")
 }
 
 hiCount("hi this is hi")
+
+/*
+* Thursday
+* Remove all 'x' from a String except if first character or last
+*
+*/
+
+func removeX(string: String) {
+  var stringCopy = ""
+  var i = 0
+  for char in string {
+    if char == "x" {
+      if i == 0 || i == count(string) {
+        stringCopy.append(char)
+        i++
+      }
+      i++
+    } else {
+      stringCopy.append(char)
+      i++
+    }
+  }
+  println(stringCopy)
+}
+
+removeX("xthixs isx testing x removal ax testx")
+
+/*
+* Thursday
+* Impliment a Stack
+*
+*/
+
+class MyStack {
+  
+  private var stack = [Int]()
+  
+  func push(add: Int) {
+    stack.append(add)
+  }
+  
+  func pop() {
+    stack.removeLast()
+  }
+  
+}
+
+var stack = MyStack()
+
+stack.push(22)
+stack.push(42)
+stack.push(52)
+
+stack.pop()
+
+
